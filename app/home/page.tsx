171d5a7f7401cd4beb54ed6cb7e8cff042470c34
Camilo Gallego <kristhiancamilogm@ufps.edu.co>
2025-02-17 22:24:49 -0500

    probando comoponentes

---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Page(){
+    return (
+        <div>
+            <p>Welcome to home</p>
+          <Button>Click me</Button>
+        </div>
+    )
+}
